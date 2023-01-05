a2b0c0c93fad0ec5c6dbc15ad6a038d4edadec06
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-01-05 13:20:54 +0100

    add User and Word models with OneToMany relationship

---
+++ src/main/java/io/project/SpringTgBot/model/Word.java
@@ -0,0 +1,16 @@
+package io.project.SpringTgBot.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "words")
+@Data
+public class Word {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String english;
+    private String russian;
+}
