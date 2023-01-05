4eb4e5d8c377ee2bc2aa079803c1e4d7389d7cf1
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-01-05 12:37:48 +0100

    add "/start" command and answer

--- src/main/java/io/project/SpringTgBot/service/TelegramBot.java
+++ src/main/java/io/project/SpringTgBot/service/TelegramBot.java
@@ -3,7 +3,9 @@ package io.project.SpringTgBot.service;
 import io.project.SpringTgBot.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -26,6 +28,29 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+            if(message.equals("/start")) {
+                startCommandAnswer(chatId, update.getMessage().getChat().getFirstName());
+            }
+        }
+    }
+
+    private void startCommandAnswer(long chatId, String name) {
+        String answer = "Hello, " + name + "! Welcome to Dictionary Bot.";
+        sendAnswer(chatId, answer);
+    }
+
+    private void sendAnswer(long chatId, String answer) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(answer);
+        try {
+            execute(sendMessage);
+        }catch (TelegramApiException ex) {
+            ex.printStackTrace();
+        }
     }
 }
