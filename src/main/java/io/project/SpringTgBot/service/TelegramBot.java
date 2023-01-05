b9a994bf86c8632ea26d7c69871b3a7e483d0c09
EvgenyKuchko <kuchkozhenia@gmail.com>
2023-01-05 11:55:22 +0100

    add class implement TelegramLongPollingBot

---
+++ src/main/java/io/project/SpringTgBot/service/TelegramBot.java
@@ -0,0 +1,31 @@
+package io.project.SpringTgBot.service;
+
+import io.project.SpringTgBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig botConfig;
+
+    public TelegramBot(BotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
